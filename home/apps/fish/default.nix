561387dc1fb29dd916ae7e89fac641592899cf08
yqlbu <yqlbu@bu.edu>
2023-06-03 10:59:46 +0000

    feat(home-manager): add nix fish theme

--- home/apps/fish/default.nix
+++ home/apps/fish/default.nix
@@ -3,4 +3,5 @@
   home.packages = [ pkgs.fish ];
   xdg.configFile."fish/config.fish".text = builtins.readFile ./config.fish;
   xdg.configFile."fish/functions".source = ./functions;
+  xdg.configFile."fish/themes".source = ./themes;
 }
