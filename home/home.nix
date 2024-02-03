e654bd56bcfdb7ba0b52626013500065fcae6166
yqlbu <yqlbu@bu.edu>
2024-02-03 21:53:29 +0800

    feat(theme): add gtk themes

--- home/home.nix
+++ home/home.nix
@@ -107,4 +107,17 @@ in
     name = "Bibata-Modern-Classic";
     size = 16;
   };
+
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+  
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
+    };
+  };
 }
