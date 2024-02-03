86ac210a6bcc5c592cd371bed36ab1e37766f9fd
yqlbu <yqlbu@bu.edu>
2024-02-03 22:46:46 +0800

    feat(boot): enable i2c-dev kernel module

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -9,7 +9,7 @@
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "i2c-dev" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
