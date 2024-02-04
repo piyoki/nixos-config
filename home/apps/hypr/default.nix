1ea082e3ace336abb51738d11bf065261481898d
yqlbu <yqlbu@bu.edu>
2024-02-04 12:08:27 +0800

    patch(app/hypr): sync remote

--- home/apps/hypr/default.nix
+++ home/apps/hypr/default.nix
@@ -4,8 +4,8 @@ let
   repo = pkgs.fetchFromGitHub {
     owner = "yqlbu";
     repo = "dot-hypr";
-    rev = "35cc230c93b9892d8b2b4837e27c8c2751e42a48";
-    sha256 = "1bqifdqk2r36hq0x6y540andmvdsgzdlcabfs1v4qhk8qjag6c5h";
+    rev = "56c50104b0c771f54c45be1b970170a8490c50c6";
+    sha256 = "1jdgnxzq1qzzppwbzpa5mf0cx5svpnzimhk115ajpll2qq3pcxm6";
   };
 in
 {
