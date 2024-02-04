c4665a30779eea6d8e678411350eb08b5976ded2
yqlbu <yqlbu@bu.edu>
2024-02-04 10:40:58 +0800

    feat: fetch hypr configs from remote

--- home/apps/hypr/default.nix
+++ home/apps/hypr/default.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
 
+let
+  repo = pkgs.fetchFromGitHub {
+    owner = "yqlbu";
+    repo = "dot-hypr";
+    rev = "91487fffda5667f613a747fc5bb002e71a5eb4fd";
+    sha256 = "0s1p95vsglyskjvd5zxxb31r9ymz366sjhkhfq1kpg52np2fdrx5";
+  };
+in
 {
-  home.packages = [ pkgs.lazygit ];
-  xdg.configFile."hypr/hyprland.conf".text = builtins.readFile ./hyprland.conf;
+  xdg.configFile."hypr".source = (repo + "/");
 }
