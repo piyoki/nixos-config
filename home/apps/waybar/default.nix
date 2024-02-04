5cf9b67c36a9d3f9efa81c3458097213f15d4d7d
yqlbu <yqlbu@bu.edu>
2024-02-04 09:49:46 +0800

    patch(app/waybar): fetch config from dot-waybar

--- home/apps/waybar/default.nix
+++ home/apps/waybar/default.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }:
 
+let
+  repo = pkgs.fetchFromGitHub {
+    owner = "yqlbu";
+    repo = "dot-waybar";
+    rev = "33262ec3dca3ce33b02ba8dc70a7ec37424863bf";
+    sha256 = "1dr2g6vrlpcy69zlp9hzk85gyf2fmra6w017ga8bjn7dhybgwbrp";
+  };
+in
 {
-  xdg.configFile."waybar/config".text = builtins.readFile ./config;
-  xdg.configFile."waybar/style.css".text = builtins.readFile ./style.css;
-  xdg.configFile."waybar/launch.sh".text = builtins.readFile ./launch.sh;
+  xdg.configFile."waybar".source = (repo + "/");
 }
