40736717fb95a8ee92b7ea0e83bb7f2cc59d532b
yqlbu <yqlbu@bu.edu>
2024-02-05 17:57:04 +0800

    feat(home/apps): persist dunst config from git remote

--- home/apps/dunst/default.nix
+++ home/apps/dunst/default.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }:
 
+let
+  repo = pkgs.fetchFromGitHub {
+    owner = "yqlbu";
+    repo = "dot-dunst";
+    rev = "a221179300189a1767bc32050add7a8009350771";
+    sha256 = "0phniq0haqd6nbxmw1bzm9xkv01799w4q07z70c0xm7k1lx658mf";
+  };
+in
 {
-  xdg.configFile."dunst/dunstrc".text = builtins.readFile ./dunstrc;
+  xdg.configFile."dunst".source = (repo + "/");
 }
