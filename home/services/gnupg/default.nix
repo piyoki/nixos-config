1070b89f47189e0c6b924caa6f508a0f3deec00a
yqlbu <yqlbu@bu.edu>
2024-02-05 21:21:34 +0800

    feat(home/service/gnupg): add sops,age

--- home/services/gnupg/default.nix
+++ home/services/gnupg/default.nix
@@ -2,6 +2,8 @@
 
 {
   home.packages = with pkgs; [
+    sops # Simple and flexible tool for managing secrets
+    age # Modern encryption tool with small explicit keys
     age-plugin-yubikey # YubiKey plugin for age
     yubikey-manager # Command line tool for configuring any YubiKey over all USB transports
     yubioath-flutter # Yubico Authenticator for Desktop
