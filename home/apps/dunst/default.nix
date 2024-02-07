9ea790ca659f78c49edf5397f8d49d0c58e770bf
yqlbu <yqlbu@bu.edu>
2024-02-07 20:27:54 +0800

    patch(apps/dunst): sync remote

--- home/apps/dunst/default.nix
+++ home/apps/dunst/default.nix
@@ -4,8 +4,8 @@ let
   repo = pkgs.fetchFromGitHub {
     owner = "yqlbu";
     repo = "dot-dunst";
-    rev = "a221179300189a1767bc32050add7a8009350771";
-    sha256 = "0phniq0haqd6nbxmw1bzm9xkv01799w4q07z70c0xm7k1lx658mf";
+    rev = "a1576e98dfc2f1563eb9ebfde5a6784d7a7a266d";
+    sha256 = "0mzbba06bc59h3ggyxpvdslx4a2da5s9w09wayivcfrfw6h33l3d";
   };
 in
 {
