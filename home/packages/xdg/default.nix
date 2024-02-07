beaf12f4ff61d0b7e5c4bab2b340213d0f32adf1
kev <31861128+yqlbu@users.noreply.github.com>
2024-02-07 22:41:56 +0800

    patch(pkgs): install wayland native pkgs from source (#12)

--- home/packages/xdg/default.nix
+++ home/packages/xdg/default.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
     xdg-utils
     xdg-user-dirs # run: xdg-user-dirs-update
+    inputs.nixpkgs-wayland.packages.${system}.waybar
   ];
 }
