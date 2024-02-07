4d9e7fa4a7f41f06eb48d7462aa90688e4db6a4a
kev <31861128+yqlbu@users.noreply.github.com>
2024-02-07 20:32:12 +0800

    refactor: relocate themes-related config (#10)

---
+++ themes/gtk/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
+    };
+
+    font = {
+      name = "Cantarell Regular";
+      size = 12;
+    };
+  };
+}
