d66c9a3e55dc76cb7c3075099014d091efdcc8a4
yqlbu <yqlbu@bu.edu>
2024-02-08 14:04:10 +0800

    patch(app/dunst): sync upstream

--- home/apps/dunst/default.nix
+++ home/apps/dunst/default.nix
@@ -4,8 +4,8 @@ let
   repo = pkgs.fetchFromGitHub {
     owner = "yqlbu";
     repo = "dot-dunst";
-    rev = "a1576e98dfc2f1563eb9ebfde5a6784d7a7a266d";
-    sha256 = "0mzbba06bc59h3ggyxpvdslx4a2da5s9w09wayivcfrfw6h33l3d";
+    rev = "aaa666bcf31b2b4530c0ed3fcdc7c0d88d173e14";
+    sha256 = "1009f2y5sf9f078b7wiyr5gxqzndp64xhsjrkapwbjarfv4jhdk8";
   };
 in
 {
