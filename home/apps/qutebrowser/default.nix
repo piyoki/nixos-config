8bcc1fa4dc76b6f1db273194b4cf4e8d72b4d9e3
yqlbu <yqlbu@bu.edu>
2024-02-08 12:49:34 +0800

    feat(app/qutebrowser): persist default theme

--- home/apps/qutebrowser/default.nix
+++ home/apps/qutebrowser/default.nix
@@ -4,10 +4,17 @@ let
   repo = pkgs.fetchFromGitHub {
     owner = "yqlbu";
     repo = "dot-qutebrowser";
-    rev = "2f71794504417a8a4cfded9939f44e97ad6cab89";
-    sha256 = "124914raz6b8nkk5imd62w06i13cyl3bxnq9rq0dz78js1yrky11";
+    rev = "5e3eb770f68bb17ee1dcb09dfe8f4d8723b22a6f";
+    sha256 = "0w5cj0piqvxb5lb5x7syfr000lmz88warnrycvbpd07lc7f0m13p";
+  };
+  theme = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "qutebrowser";
+    rev = "78bb72b4c60b421c8ea64dd7c960add6add92f83";
+    sha256 = "0j47m5swip84c1iwr6nhidadk20jbncqmlarbwrabqc3idcwg7ln";
   };
 in
 {
   xdg.configFile."qutebrowser/config.py".source = (repo + "/config.py");
+  xdg.configFile."qutebrowser/catppuccin".source = (theme + "/");
 }
