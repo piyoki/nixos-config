028ad1ae8571d0a0b5eaf5aceb49f1a8487c9366
yqlbu <yqlbu@bu.edu>
2024-02-08 22:33:52 +0800

    feat(app): persist lazygit,lf configs

--- home/apps/lazygit/default.nix
+++ home/apps/lazygit/default.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
 
+let
+  repo = pkgs.fetchFromGitHub {
+    owner = "yqlbu";
+    repo = "dot-qutebrowser";
+    rev = "067cc94d41624e32ea52632d8eaad91be64f1c25";
+    sha256 = "1ai7nhbcccip9a242p82ky2iyhn7i0rv6350qmc5bnh81b0hgxdf";
+  };
+in
 {
-  home.packages = [ pkgs.lazygit ];
-  xdg.configFile."lazygit/config.yml".text = builtins.readFile ./config.yml;
+  xdg.configFile."lazygit/config.yml".source = (repo + "/config.yml");
 }
