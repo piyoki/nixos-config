c57e64bdb47c3a1c168490d6d7bf533ec672be12
yqlbu <yqlbu@bu.edu>
2024-02-09 00:47:22 +0800

    feat(home/pkgs): add mpd

--- home/packages/windowmanager/hyprland/system/default.nix
+++ home/packages/windowmanager/hyprland/system/default.nix
@@ -6,12 +6,13 @@
     inputs.nixpkgs-wayland.packages.${system}.waybar # wayland-based bar/dock
     inputs.nixpkgs-wayland.packages.${system}.swww # Efficient animated wallpaper daemon for wayland, controlled at runtime
     cliphist # clipboard manager
+    dunst # notification daemon
     ffmpeg
     ffmpegthumbs
-    xfce.tumbler  # thumbnailer service
-    dunst # notification daemon
     gnome.file-roller # archive manager
+    mpd # music daemon
     networkmanagerapplet # network manager (gtk GUI)
     swayidle # idle management daemon
+    xfce.tumbler  # thumbnailer service
   ];
 }
