6c90a4a49e36627a14275b714fd3189ae4cd21d8
yqlbu <yqlbu@bu.edu>
2024-02-08 22:09:42 +0800

    chore(app/waybar): sync remote

--- home/apps/waybar/default.nix
+++ home/apps/waybar/default.nix
@@ -4,8 +4,8 @@ let
   repo = pkgs.fetchFromGitHub {
     owner = "yqlbu";
     repo = "dot-waybar";
-    rev = "3e42cb71901523d33b3edf81068f767feb188042";
-    sha256 = "07w1g4z0l3g0ij63xhkvrvxhdvkzy1h0808yaq9hdmsdxpglm5j8";
+    rev = "8b4948965f961077a6a2c5199160293e39671cee";
+    sha256 = "1wnbkljd8h1yahmia3vgmw0gsazl5jvd0akb38ahwakiy6fwb7fg";
   };
 in
 {
