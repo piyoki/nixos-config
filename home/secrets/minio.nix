a3e505a5626838554701af13c0a712ebaa38e494
yqlbu <yqlbu@bu.edu>
2024-02-08 21:45:18 +0800

    patch(secrets/minio): update secret ext

--- home/secrets/minio.nix
+++ home/secrets/minio.nix
@@ -5,7 +5,7 @@
     "minio/config" = {
       sopsFile = ../../../secrets/minio.enc.yaml;
       mode = "0600";
-      path = "${config.home.homeDirectory}/.mc/config";
+      path = "${config.home.homeDirectory}/.mc/config.json";
     };
   };
 }
