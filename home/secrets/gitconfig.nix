da2421bf8cb249d1d0d57837b8035113742bff98
kev <31861128+yqlbu@users.noreply.github.com>
2024-02-09 21:58:12 +0800

    feat/refactor(secret): add secrets as submodule from remote (#17)
    * feat: add secrets as submodule from remote
    
    * patch(secret): update gitconfig secret ref
    
    * patch: udpate update abbrs

--- home/secrets/gitconfig.nix
+++ home/secrets/gitconfig.nix
@@ -4,22 +4,22 @@
   sops.secrets = {
     # git
     "gitconfig/general" = {
-      sopsFile = "${inputs.secrets}/gitconfig.enc.yaml";
+      sopsFile = "${inputs.secrets}/nix.gitconfig.enc.yaml";
       mode = "0600";
       path = "${config.home.homeDirectory}/.gitconfig";
     };
     "gitconfig/profile/personal" = {
-      sopsFile = "${inputs.secrets}/gitconfig.enc.yaml";
+      sopsFile = "${inputs.secrets}/nix.gitconfig.enc.yaml";
       mode = "0600";
       path = "${config.home.homeDirectory}/.gitconfigs/.gitconfig.personal";
     };
     "gitconfig/profile/work" = {
-      sopsFile = "${inputs.secrets}/gitconfig.enc.yaml";
+      sopsFile = "${inputs.secrets}/nix.gitconfig.enc.yaml";
       mode = "0600";
       path = "${config.home.homeDirectory}/.gitconfigs/.gitconfig.work";
     };
     "gitconfig/profile/extras" = {
-      sopsFile = "${inputs.secrets}/gitconfig.enc.yaml";
+      sopsFile = "${inputs.secrets}/nix.gitconfig.enc.yaml";
       mode = "0600";
       path = "${config.home.homeDirectory}/.gitconfigs/.gitconfig.extras";
     };
