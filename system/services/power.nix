25fdb6c3196d6c961ce5f7ffdd6f4bbe646694b8
kev <31861128+yqlbu@users.noreply.github.com>
2024-02-11 14:27:50 +0800

    feat(system): persist powermanagement settings (#22)

---
+++ system/services/power.nix
@@ -0,0 +1,32 @@
+{ ... }:
+
+# References:
+# https://nixos.wiki/wiki/Laptop
+# https://nixos.wiki/wiki/Power_Management
+{
+  powerManagement.enable = true;
+
+  # prevents overheating on Intel CPUs
+  services.thermald.enable = true;
+  # power-saving setttings
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 60;
+
+      #Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+    };
+  };
+}
+
