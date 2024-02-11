259b81ac62c298eb9550eb14e703a14641a991de
yqlbu <yqlbu@bu.edu>
2024-02-11 20:17:23 +0800

    feat(template/app/from_file): add home file example

--- templates/app/from_file.nix
+++ templates/app/from_file.nix
@@ -1,7 +1,11 @@
 { inputs, pkgs, ... }:
 
 {
+  # $HOME/.config/
   xdg.configFile."path/file".text = builtins.readFile ./file;
   xdg.configFile."path/dir".source = ./dir;
+
+  # $HOME
+  home.file."path/dir".source = ./dir;
 }
 
