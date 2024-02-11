dd5b701bb72f8b921cb8c49bf0803a3b7567ab50
yqlbu <yqlbu@bu.edu>
2024-02-11 21:35:04 +0800

    patch(home/secrets/gitconfig): loop secret injection

--- home/secrets/gitconfig.nix
+++ home/secrets/gitconfig.nix
@@ -1,27 +1,14 @@
 { inputs, config, ... }:
 
 {
-  sops.secrets = {
-    # git
-    "gitconfig/general" = {
-      sopsFile = "${inputs.secrets}/nix.gitconfig.enc.yaml";
-      mode = "0600";
-      path = "${config.home.homeDirectory}/.gitconfig";
-    };
-    "gitconfig/profile/personal" = {
-      sopsFile = "${inputs.secrets}/nix.gitconfig.enc.yaml";
-      mode = "0600";
-      path = "${config.home.homeDirectory}/.gitconfigs/.gitconfig.personal";
-    };
-    "gitconfig/profile/work" = {
-      sopsFile = "${inputs.secrets}/nix.gitconfig.enc.yaml";
-      mode = "0600";
-      path = "${config.home.homeDirectory}/.gitconfigs/.gitconfig.work";
-    };
-    "gitconfig/profile/extras" = {
-      sopsFile = "${inputs.secrets}/nix.gitconfig.enc.yaml";
-      mode = "0600";
-      path = "${config.home.homeDirectory}/.gitconfigs/.gitconfig.extras";
-    };
+  # git
+  sops.secrets = builtins.mapAttrs (_name: value: value // {
+    sopsFile = "${inputs.secrets}/nix.gitconfig.enc.yaml";
+    mode = "0600";
+  }) {
+    "gitconfig/general".path = "${config.home.homeDirectory}/.gitconfig";
+    "gitconfig/profile/personal".path = "${config.home.homeDirectory}/.gitconfigs/.gitconfig.personal";
+    "gitconfig/profile/work".path = "${config.home.homeDirectory}/.gitconfigs/.gitconfig.work";
+    "gitconfig/profile/extras".path = "${config.home.homeDirectory}/.gitconfigs/.gitconfig.extras";
   };
 }
