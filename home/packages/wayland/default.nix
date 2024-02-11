d56a67af1446ea3ed44c49337be263cdd1cb1980
yqlbu <yqlbu@bu.edu>
2024-02-11 10:15:50 +0800

    feat(home/pkg): add xdg-desktop-portal-wlr

--- home/packages/wayland/default.nix
+++ home/packages/wayland/default.nix
@@ -4,5 +4,6 @@
   home.packages = with pkgs; [
     inputs.nixpkgs-wayland.packages.${system}.wdisplays # A graphical application for configuring displays in Wayland compositors
     inputs.nixpkgs-wayland.packages.${system}.wl-clipboard # Command-line copy/paste utilities for Wayland
+    inputs.nixpkgs-wayland.packages.${system}.xdg-desktop-portal-wlr # xdg-desktop-portal backend for wlroots
   ];
 }
--- home/packages/xdg/default.nix
+++ home/packages/xdg/default.nix
@@ -1,9 +1,9 @@
-{ config, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
     xdg-utils # provides cli tools such as `xdg-mime` `xdg-open`
-    xdg-user-dirs
+    xdg-user-dirs # A tool to help manage well known user directories like the desktop folder and the music folder
   ];
 
   xdg = {
