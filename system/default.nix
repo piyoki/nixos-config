b70dab91f2a707b593801b2ae7942d94d8f70f29
yqlbu <yqlbu@bu.edu>
2024-02-12 20:28:38 +0800

    feat(system/pkg): add statix

--- system/default.nix
+++ system/default.nix
@@ -16,6 +16,7 @@
     nixpkgs-fmt # Nix code formatter for nixpkgs
     nix-prefetch-scripts # Collection of all the nix-prefetch-* scripts which may be used to obtain source hashes
     update-nix-fetchgit # A program to update fetchgit values in Nix expressions
+    statix # Lints and suggestions for the Nix programming language
 
     # basic but essential
     gnutar
