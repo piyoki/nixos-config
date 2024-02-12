54374edc47c4b4f31a4b2a9460cf8698b399c86b
yqlbu <yqlbu@bu.edu>
2024-02-12 21:24:12 +0800

    feat(app/fish): sync completions from upstream

--- home/apps/fish/default.nix
+++ home/apps/fish/default.nix
@@ -6,5 +6,6 @@
   xdg.configFile."fish/config.fish".source = "${inputs.fish}/config.fish";
   xdg.configFile."fish/config.d".source = "${inputs.fish}/config.d";
   xdg.configFile."fish/functions".source = "${inputs.fish}/functions";
+  xdg.configFile."fish/completions".source = "${inputs.fish}/completions";
   xdg.configFile."fish/themes".source = "${inputs.fish}/themes";
 }
