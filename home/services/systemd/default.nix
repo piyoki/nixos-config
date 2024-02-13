3bca3cada28c1aae4d5584e8f8935d82fda29f23
yqlbu <yqlbu@bu.edu>
2024-02-13 19:24:29 +0800

    feat(home/services): add systemd hooks

---
+++ home/services/systemd/default.nix
@@ -0,0 +1,9 @@
+_:
+
+{
+  imports = [
+    ./cron
+    ./hooks
+  ];
+}
+
---
+++ home/services/systemd/hooks/powermanagement/default.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, user, ... }:
+
+{
+  # power resume hooks
+  # relaunch waybars
+  systemd.user.services."relaunch-waybars" = {
+    Unit = {
+      Description = "relaunch waybars when power resumes";
+      After = [ "post-resume.target" ];
+    };
+    Install = {
+      WantedBy = [ "post-resume.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.writeShellScript "relaunch-waybars" ''
+        set -eux
+        /run/current-system/sw/bin/pkill .waybar-wrapped
+        /run/current-system/sw/bin/sleep 2
+        # FIXME LATER
+        # ${pkgs.waybar}/bin/waybar -c $CONFIG_DIR/config.json -s $CONFIG_DIR/style.css &
+      ''}";
+      Type = "oneshot";
+      Environment = "CONFIG_DIR=${config.home.homeDirectory}/.config/waybar";
+    };
+  };
+
+  # Usage
+  # manually run a service once for testing purposes:
+  # $ systemctl start --user <service>
+}
