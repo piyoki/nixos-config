4ff07965751a7343400449cec045c9fee65d3a77
yqlbu <yqlbu@bu.edu>
2024-02-13 20:52:22 +0800

    patch(systemd/hooks): use killall instead of pkill

--- home/services/systemd/hooks/powermanagement/default.nix
+++ home/services/systemd/hooks/powermanagement/default.nix
@@ -15,7 +15,7 @@
     Service = {
       ExecStart = "${pkgs.writeShellScript "relaunch-waybars" ''
         set -eux
-        /run/current-system/sw/bin/pkill .waybar-wrapped
+        /run/current-system/sw/bin/killall .waybar-wrapped
         /run/current-system/sw/bin/sleep 2
         # FIXME LATER
         # ${pkgs.waybar}/bin/waybar -c $CONFIG_DIR/config.json -s $CONFIG_DIR/style.css &
