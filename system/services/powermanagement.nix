d3be412d52b74c0b92a350aa67ba6eef37544092
kev <31861128+yqlbu@users.noreply.github.com>
2024-02-17 01:32:26 +0800

    patch(sysytem/service): adjust power settings (#44)

--- system/services/powermanagement.nix
+++ system/services/powermanagement.nix
@@ -18,18 +18,18 @@
     tlp = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        # CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        # CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        # CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 60;
+        # CPU_MIN_PERF_ON_AC = 0;
+        # CPU_MAX_PERF_ON_AC = 100;
+        # CPU_MIN_PERF_ON_BAT = 0;
+        # CPU_MAX_PERF_ON_BAT = 60;
 
-        # (optional) helps save long term battery health
+        # helps save long term battery health
         START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
         STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
 
@@ -45,11 +45,13 @@
       settings = {
         battery = {
           governor = "powersave";
+          energy_performance_preference = "power";
           turbo = "never";
         };
         charger = {
           governor = "performance";
           turbo = "auto";
+          energy_performance_preference = "performance";
         };
       };
     };
