87ec302370fd9978ed8484b4d5587604fc7ee087
yqlbu <yqlbu@bu.edu>
2024-02-16 14:03:30 +0800

    feat(home/pkg): add productive devops pkgs

--- home/packages/devops/default.nix
+++ home/packages/devops/default.nix
@@ -2,7 +2,20 @@
 
 {
   home.packages = with pkgs; [
+    # ansible
     ansible
+
+    # docker
+    docker-compose
+    lazydocker # Docker terminal UI.
+    # dive # A tool for exploring each layer in a docker image
+
+    # kubernetes
+    kubectl
+    kubernetes-helm
+    k9s # TUI for k8s
+
+    # gitops
+    fluxcd # Open and extensible continuous delivery solution for Kubernetes
   ];
 }
-
