ee9eaa03c9bbb1364f57329fefaf2798cc60bba1
kev <31861128+yqlbu@users.noreply.github.com>
2024-02-16 13:54:35 +0800

    feat(system/service): add cifs-mount module (#42)

---
+++ system/services/cifs-mount.nix
@@ -0,0 +1,26 @@
+{ user, ... }:
+
+let
+  host = "10.178.0.81";
+  homeDir = "/home/${user}";
+  mountOpts = [
+    # https://www.freedesktop.org/software/systemd/man/latest/systemd.mount.html
+    "nofail,_netdev"
+    "uid=1000,gid=100,dir_mode=0755,file_mode=0755,iocharset=utf8"
+    "vers=3.0,credentials=${homeDir}/.smbcredentials"
+  ];
+  fsType = "cifs";
+in
+{
+  # mount smb/cifs share
+  fileSystems."${homeDir}/Tank" = {
+    device = "//${host}/Tank";
+    inherit fsType;
+    options = mountOpts;
+  };
+  fileSystems."${homeDir}/Media" = {
+    device = "//${host}/Media";
+    inherit fsType;
+    options = mountOpts;
+  };
+}
