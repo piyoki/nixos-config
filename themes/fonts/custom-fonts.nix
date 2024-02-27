cfb4a8fef4250f1842ab4efbebbe079284b01db1
kev <yqlbu@bu.edu>
2024-02-27 22:33:01 +0800

    feat(system/fonts): add comic-code-ligatures

--- themes/fonts/custom-fonts.nix
+++ themes/fonts/custom-fonts.nix
@@ -6,6 +6,7 @@
     packages = with pkgs; [
       inputs.nur.packages.${system}.genseki-gothic
       inputs.nur.packages.${system}.comic-code
+      inputs.nur.packages.${system}.comic-code-ligatures
       inputs.nur.packages.${system}.vt323
     ];
   };
