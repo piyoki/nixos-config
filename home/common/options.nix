6fd28d7ccc5cd2e289d88d781d1b4b7d36b8d64b
kev <yqlbu@bu.edu>
2024-03-03 01:07:20 +0800

    chore(home/common/option): add reference

--- home/common/options.nix
+++ home/common/options.nix
@@ -2,6 +2,9 @@
 
 with lib;
 
+# References:
+# https://nixos.wiki/wiki/Declaration
+# https://www.reddit.com/r/NixOS/comments/wzsz4k/how_can_i_add_some_custom_data_variables_into/
 {
   options.common = {
     profile = mkOption {
