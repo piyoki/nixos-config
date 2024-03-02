52d948cfa79c8e549255606031f454995d225a53
kev <yqlbu@bu.edu>
2024-03-02 21:41:37 +0800

    chore(flake): switch to unstable, update upstream inputs

--- flake.nix
+++ flake.nix
@@ -65,7 +65,7 @@
 
   inputs = {
     # public source
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
