511073c4e4f43dca2ea793df2710d293615f81d2
kev <yqlbu@bu.edu>
2024-03-03 17:15:55 +0800

    feat(system/pkg): add nix-output-monitor

--- system/packages/nixos.nix
+++ system/packages/nixos.nix
@@ -8,5 +8,6 @@
     update-nix-fetchgit # A program to update fetchgit values in Nix expressions
     statix # Lints and suggestions for the Nix programming language
     deadnix # Find and remove unused code in .nix source files
+    nix-output-monitor # Processes output of Nix commands to show helpful and pretty information
   ];
 }
