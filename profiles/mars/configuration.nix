4860aa426c656e6e021b32f37147d976e150e428
kev <yqlbu@bu.edu>
2024-03-04 13:09:45 +0800

    refactor(profile/mars0: modularize system configs

--- profiles/mars/configuration.nix
+++ profiles/mars/configuration.nix
@@ -2,12 +2,24 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
+
+      # system modules
+      ../../system/services/fish.nix
+      ../../system/services/docker.nix
+      ../../system/services/openssh.nix
+      ../../system/services/zramd.nix
+      ../../system/internationalisation/locale.nix
+      ../../system/internationalisation/time.nix
+
+      # shared modules
+      ../../shared/nixos.nix
     ];
 
   boot.loader.systemd-boot.enable = true;
@@ -16,9 +28,6 @@
 
   networking.networkmanager.enable = true;
   networking.hostName = "nixos-mars";
-
-  time.timeZone = "Asia/Shanghai";
-  i18n.defaultLocale = "en_US.UTF-8";
 
   environment.systemPackages = with pkgs; [
     git
@@ -33,7 +42,7 @@
   users.users = {
     kev = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "networkmanager" "docker"];
+      extraGroups = [ "wheel" "networkmanager" "docker" ];
       shell = pkgs.fish;
       packages = with pkgs; [ fish ];
     };
@@ -42,44 +51,6 @@
       shell = pkgs.bash;
     };
   };
-  users.extraGroups.docker.members = [ "kev" ];
-
-  programs.fish.enable = true;
-
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "no"; # disable root login
-      PasswordAuthentication = true; # disable password login 
-    };
-  };
-
-  zramSwap.enable = true;
-
-  networking.firewall.allowedTCPPorts = [ 22 ];
 
   environment.variables.EDITOR = "vim";
-
-  virtualisation.docker = {
-    enable = true;
-    enableOnBoot = true;
-  };
-
-  nix = {
-    settings = {
-      max-jobs = lib.mkDefault 8;
-      auto-optimise-store = true;
-      experimental-features = [ "nix-command" "flakes" ];
-
-    };
-    gc = {
-      automatic = lib.mkDefault true;
-      dates = lib.mkDefault "weekly";
-      options = lib.mkDefault "--delete-older-than 1w";
-    };
-  };
-
-  system.stateVersion = "24.11";
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
