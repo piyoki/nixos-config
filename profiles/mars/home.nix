49f4f928aaf9e7189b20aaa924d2de61080990a4
kev <yqlbu@bu.edu>
2024-03-04 14:15:41 +0800

    feat: load lazygit,tmux,bat configs

--- profiles/mars/home.nix
+++ profiles/mars/home.nix
@@ -4,6 +4,9 @@
   imports = [
     # host specific modules
     ../../home/apps/fish
+    ../../home/apps/tmux
+    ../../home/apps/lazygit
+    ../../home/apps/bat
     ../../home/services/encryption/server.nix
 
     # shared modules
