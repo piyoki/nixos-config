06e7e3bb068c0300e0258fc24a822fa8cbdeda59
kev <yqlbu@bu.edu>
2024-03-06 21:20:03 +0800

    patch(shared/home): add conf/

--- shared/modules/home/gnupg.nix
+++ shared/modules/home/gnupg.nix
@@ -4,7 +4,7 @@ path:
   home = {
     file = {
       # gnupg
-      ".gnupg/scdaemon.conf".text = builtins.readFile ./scdaemon.conf;
+      ".gnupg/scdaemon.conf".text = builtins.readFile ./conf/scdaemon.conf;
       ".gnupg/gpg.conf".text = builtins.readFile path;
     };
   };
