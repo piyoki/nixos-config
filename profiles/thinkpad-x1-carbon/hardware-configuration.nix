1618a9e87e794c9babcfa5af3ffc02613ad67a41
kev <yqlbu@bu.edu>
2024-03-06 21:16:54 +0800

    patch(profiles): enable tmp.cleanOnBoot

--- profiles/thinkpad-x1-carbon/hardware-configuration.nix
+++ profiles/thinkpad-x1-carbon/hardware-configuration.nix
@@ -48,6 +48,8 @@
       options snd_hda_intel power_save=1 power_save_controller=Y
       options i915 enable_guc=1 enable_fbc=1 enable_psr=1 force_probe=7d55
     '';
+    # clear /tmp on boot to get a stateless /tmp directory.
+    tmp.cleanOnBoot = true;
   };
 
   fileSystems = {
