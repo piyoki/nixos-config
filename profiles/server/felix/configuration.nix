ee0fe81edccfd5acebd703de66258252dd7412da
kev <yqlbu@bu.edu>
2024-03-08 01:31:52 +0800

    feat(profile/felix): persist networking settings

--- profiles/server/felix/configuration.nix
+++ profiles/server/felix/configuration.nix
@@ -13,7 +13,18 @@
     # ../../../shared/modules/system/tmpfs/persistent/server.nix
   ];
 
-  networking.hostName = "nixos-felix";
+  networking = {
+    hostName = "nixos-felix";
+    interfaces.ens18 = {
+      useDHCP = false;
+      ipv4.addresses = [{
+        address = "10.118.25.31";
+        prefixLength = 24;
+      }];
+    };
+    defaultGateway = "10.118.25.4";
+    nameservers = [ "10.118.25.4" ];
+  };
 
   # enable qemu-guest-agent
   services.qemuGuest.enable = lib.mkDefault true;
