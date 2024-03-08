b679513dcc1ee40487b90b4666bfa2227c79220f
kev <yqlbu@bu.edu>
2024-03-08 19:49:38 +0800

    patch(profiles): persist networking settings

--- profiles/server/felix/configuration.nix
+++ profiles/server/felix/configuration.nix
@@ -13,10 +13,16 @@
     # ../../../shared/modules/system/tmpfs/persistent/server.nix
   ];
 
+  networking.hostName = "nixos-felix";
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  # networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.ens18.useDHCP = lib.mkDefault true;
   networking = {
-    hostName = "nixos-felix";
     interfaces.ens18 = {
-      useDHCP = false;
+      useDHCP = lib.mkDefault false;
       ipv4.addresses = [{
         address = "10.118.25.31";
         prefixLength = 24;
--- profiles/server/mars/configuration.nix
+++ profiles/server/mars/configuration.nix
@@ -13,7 +13,25 @@
     ../../../shared/modules/system/tmpfs/persistent/server.nix
   ];
 
-  networking.hostName = "nixos-mars";
+  networking. hostName = "nixos-felix";
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  # networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.ens18.useDHCP = lib.mkDefault true;
+  networking = {
+    interfaces.ens18 = {
+      useDHCP = lib.mkDefault false;
+      ipv4.addresses = [{
+        address = "10.118.25.30";
+        prefixLength = 24;
+      }];
+    };
+    defaultGateway = "10.118.25.4";
+    nameservers = [ "10.118.25.4" ];
+  };
 
   # enable qemu-guest-agent
   services.qemuGuest.enable = lib.mkDefault true;
