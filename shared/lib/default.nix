c7baba9203b80cb87fb28a6782d7bd969d7b1682
kev <yqlbu@bu.edu>
2024-03-11 18:40:14 +0800

    feat(shared/lib): add scanPath func

--- shared/lib/default.nix
+++ shared/lib/default.nix
@@ -1,8 +1,20 @@
 { lib, ... }:
 
+with builtins;
 {
   sharedLib = {
     # (lib.path.append): Given a directory, return a flattened list of all files within it recursively.
     relativeToRoot = lib.path.append ../../.;
+    # Scan a given directory and look for default.nix; import modules recursively
+    scanPaths = path:
+      map
+        (f: (path + "/${f}"))
+        (attrNames
+          (lib.attrsets.filterAttrs
+            (
+              _path: _type:
+                (_type == "directory") # include directories
+            )
+            (readDir path)));
   };
 }
