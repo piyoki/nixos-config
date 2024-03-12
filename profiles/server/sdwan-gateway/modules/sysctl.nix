17f99be30a954d19e19ef1b94676a93e4b302d6a
kev <31861128+yqlbu@users.noreply.github.com>
2024-03-13 02:07:20 +0800

    feat(profile/sdwan): enable tcp_fastopen (#132)

--- profiles/server/sdwan-gateway/modules/sysctl.nix
+++ profiles/server/sdwan-gateway/modules/sysctl.nix
@@ -5,6 +5,8 @@ _:
     # enable bbr
     "net.ipv4.tcp_congestion_control" = "bbr";
     "net.core.default_qdisc" = "fq";
+    # enable tcp_fastopen
+    "net.ipv4.tcp_fastopen" = 3;
     # enable swapiness
     "vm.swappiness" = 60;
   };
