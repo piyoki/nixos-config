939389af0399ff12d736a176b57f9a160e9db06f
kev <yqlbu@bu.edu>
2024-03-13 02:12:09 +0800

    feat(profile/sdwan): apply throughput tweaks

--- profiles/server/sdwan-gateway/modules/sysctl.nix
+++ profiles/server/sdwan-gateway/modules/sysctl.nix
@@ -9,5 +9,13 @@ _:
     "net.ipv4.tcp_fastopen" = 3;
     # enable swapiness
     "vm.swappiness" = 60;
+    # apply throughput tweaks
+    "net.ipv4.tcp_tw_reuse" = 1;
+    "net.ipv4.tcp_tw_recycle" = 1;
+    "net.ipv4.tcp_syncookies" = 1;
+    "net.ipv4.tcp_syn_retries" = 1;
+    "net.ipv4.tcp_synack_retries" = 1;
+    "net.ipv4.tcp_timestamps" = 1;
+    "net.ipv4.tcp_fin_timeout" = 30;
   };
 }
