fb32cdeae9d75a9adf3a827a9e4fd0592a281627
kev <yqlbu@bu.edu>
2024-03-14 00:11:23 +0800

    chore(shared/server/system/patches/users-shell-bash): remove unnecessary line

--- shared/server/system/patches/users-shell-bash.nix
+++ shared/server/system/patches/users-shell-bash.nix
@@ -6,6 +6,5 @@ with lib;
   users.users.${user} = {
     shell = mkForce pkgs.bash;
     extraGroups = mkForce [ "wheel" ];
-    hashedPassword = "$7$CU..../....nnOYNef.N4rHN9q8TseVo1$cgf5w2iAkrNXxU1uwGI0HlFFGuwcU3l507b67v0Kp49";
   };
 }
