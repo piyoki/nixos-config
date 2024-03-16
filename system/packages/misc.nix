6a588612b165e39bd1b2f97a152ffbf24ca331f8
kev <yqlbu@bu.edu>
2024-03-17 01:50:32 +0800

    feat(system/pkgs): add mesa,intel related pkgs

--- system/packages/misc.nix
+++ system/packages/misc.nix
@@ -3,10 +3,13 @@
 {
   environment.systemPackages = with pkgs; [
     # misc
-    brightnessctl
-    cifs-utils
-    dmidecode
-    libnotify
+    brightnessctl # This program allows you read and control device brightness
+    cifs-utils # Tools for managing Linux CIFS client filesystems
+    dmidecode # A tool that reads information about your system's hardware from the BIOS according to the SMBIOS/DMI standard
+    libnotify # A library that sends desktop notifications to a notification daemon
     libva-utils # A collection of utilities and examples for VA-API
+    cpufetch # Simplistic yet fancy CPU architecture fetching tool
+    vulkan-tools # Khronos official Vulkan Tools and Utilities
+    intel-gpu-tools # Tools for development and testing of the Intel DRM driver
   ];
 }
