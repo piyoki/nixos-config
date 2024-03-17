8bbb706b007d937dbfa8d4619566e2f6865d06e4
kev <yqlbu@bu.edu>
2024-03-17 10:30:18 +0800

    feat(shared/nixos): add nix-community binary cache

--- shared/nixos.nix
+++ shared/nixos.nix
@@ -21,11 +21,13 @@
         "https://nixpkgs-wayland.cachix.org"
         "https://hyprland.cachix.org"
         "https://nyx.chaotic.cx"
+        "https://nix-community.cachix.org"
       ];
       extra-trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       # ref: https://github.com/NixOS/nix/issues/4894
       # workaround to fix ssh signature issues
