7d58219da56aa2daf64ad13dc0442715cc23a137
kev <31861128+yqlbu@users.noreply.github.com>
2024-03-18 19:56:42 +0800

    Revert "optimize(profiles): use lto-kernel for better performance (#162)" (#163)
    This reverts commit 9fc9c3e22ffa0bf676ca7a8b53f6dcddbda7712e.

--- profiles/workstation/nuc-12/hardware-configuration.nix
+++ profiles/workstation/nuc-12/hardware-configuration.nix
@@ -13,7 +13,7 @@
 
   boot = {
     # Use the systemd-boot EFI boot loader.
-    kernelPackages = inputs.chaotic.packages.${system}.linuxPackages_cachyos-lto;
+    kernelPackages = inputs.chaotic.packages.${system}.linuxPackages_cachyos;
     supportedFilesystems = [ "ext4" "btrfs" "xfs" "fat" "vfat" "cifs" "nfs" ];
 
     loader = {
