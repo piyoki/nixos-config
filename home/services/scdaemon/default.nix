02aca702e7b797ce115ea6939db5eebb218df8be
kev <yqlbu@bu.edu>
2024-03-19 21:10:29 +0800

    refactor(home/services): decouple scdaemon module from systemd

---
+++ home/services/scdaemon/default.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+# $HOME/.config/systemd/user/<name>.{time,service}
+{
+  # systemd timer
+  systemd.user.timers."restart-scdaemon" = {
+    Unit = {
+      Description = "Systemd timer for restart-scdaemon.service, run every 1h";
+    };
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
+    Timer = {
+      OnBootSec = "1h";
+      OnUnitActiveSec = "1h";
+    };
+  };
+
+  # systemd service
+  systemd.user.services."restart-scdaemon" = {
+    Unit = {
+      Description = "Restart gnupg scdaemon";
+    };
+    Service = {
+      ExecStart = "${pkgs.writeShellScript "restart-scdaemon" ''
+        set -eu
+        /run/current-system/sw/bin/killall gpg-agent
+        /run/current-system/sw/bin/gpgconf --reload scdaemon
+      ''}";
+      Type = "oneshot";
+    };
+  };
+}
