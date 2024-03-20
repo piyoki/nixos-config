c311fabd941ab14daf5cb55af14d71c352a5f4bf
kev <yqlbu@bu.edu>
2024-03-20 09:17:57 +0800

    patch(system/server/patches): apply extra sysctl tweaks

--- shared/server/system/patches/sysctl-networking-tweaks.nix
+++ shared/server/system/patches/sysctl-networking-tweaks.nix
@@ -3,6 +3,7 @@ _:
 # References:
 # https://cloud.tencent.com/developer/article/2141920
 # https://github.com/3ayonara/LinuxTools/blob/main/Network/BBR/sysctl.conf
+# https://wiki.archlinuxcn.org/zh-hk/Sysctl (Recommended)
 {
   boot.kernel.sysctl = {
     # enable ip_forward
@@ -16,19 +17,24 @@ _:
     # enable swapiness
     "vm.swappiness" = 60;
     # apply throughput tweaks
+    "net.ipv4.tcp_mtu_probing" = 1;
     "net.ipv4.tcp_tw_reuse" = 1;
     "net.ipv4.tcp_tw_recycle" = 1;
     "net.ipv4.tcp_syncookies" = 1;
     "net.ipv4.tcp_syn_retries" = 2;
     "net.ipv4.tcp_synack_retries" = 2;
-    "net.ipv4.tcp_timestsmps" = 0;
+    "net.ipv4.tcp_timestamps" = 0;
     "net.ipv4.tcp_fin_timeout" = 30;
+    "net.ipv4.tcp_slow_start_after_idle" = 0;
     "net.ipv4.tcp_max_syn_backlog" = 8192;
     "net.ipv4.tcp_max_tw_buckets" = 5000;
+    "net.core.optmem_max" = 65536;
+    "net.core.rmem_default" = 1048576;
+    "net.core.wmem_default" = 1048576;
     "net.core.rmem_max" = 16777216;
     "net.core.wmem_max" = 16777216;
-    "net.ipv4.tcp_keepalive_intvl" = 3;
-    "net.ipv4.tcp_keepalive_probes" = 5;
     "net.ipv4.tcp_keepalive_time" = 1200;
+    "net.ipv4.tcp_keepalive_intvl" = 10;
+    "net.ipv4.tcp_keepalive_probes" = 6;
   };
 }
