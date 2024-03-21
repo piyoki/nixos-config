9446ad5f18e762650cdfaa38d9576ae9e231b9ec
kev <yqlbu@bu.edu>
2024-03-22 02:54:35 +0800

    feat(home/app): add webcord; demise vesktop

--- home/packages/windowmanager/hyprland/im/default.nix
+++ home/packages/windowmanager/hyprland/im/default.nix
@@ -4,6 +4,18 @@
   home.packages = with pkgs; [
     inputs.chaotic.packages.${system}.telegram-desktop_git
     cinny-desktop # Yet another matrix client for desktop
-    vesktop # An alternate client for Discord with Vencord built-in
+    webcord # A Discord and SpaceBar electron-based client implemented without Discord API
+    # (webcord.overrideAttrs (oldAttrs: {
+    #   desktopItems = [
+    #     (makeDesktopItem {
+    #       name = "webcord";
+    #       exec = "webcord --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto --enable-wayland-ime --enable-features=WebRTCPipeWireCapturer";
+    #       icon = "webcord";
+    #       desktopName = "WebCord";
+    #       comment = oldAttrs.meta.description;
+    #       categories = [ "Network" "InstantMessaging" ];
+    #     })
+    #   ];
+    # }))
   ];
 }
