443fd0d0ac123e9bd266c98f9f1f57fc11a5b30c
kev <31861128+yqlbu@users.noreply.github.com>
2024-03-21 23:18:05 +0800

    feat(home/pkgs): add vesktop (#172)

--- home/packages/windowmanager/hyprland/im/default.nix
+++ home/packages/windowmanager/hyprland/im/default.nix
@@ -4,5 +4,6 @@
   home.packages = with pkgs; [
     inputs.chaotic.packages.${system}.telegram-desktop_git
     cinny-desktop # Yet another matrix client for desktop
+    vesktop # An alternate client for Discord with Vencord built-in
   ];
 }
