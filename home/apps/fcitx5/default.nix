a5e0c54109ec1e3e3264663c39ed4c8e6823e8ff
kev <yqlbu@bu.edu>
2024-03-27 21:02:07 +0800

    feat(home/app): persist fcitx5 settings

---
+++ home/apps/fcitx5/default.nix
@@ -0,0 +1,9 @@
+_:
+
+{
+  xdg.configFile."fcitx5/profile".source = ./profile;
+  xdg.configFile."fcitx5/conf/classicui.conf".source = ./conf/classicui.conf;
+  xdg.configFile."fcitx5/conf/pinyin.conf".source = ./conf/pinyin.conf;
+  xdg.configFile."fcitx5/conf/cloudpinyin.conf".source = ./conf/cloudpinyin.conf;
+  xdg.configFile."fcitx5/conf/chttrans.conf".source = ./conf/chttrans.conf;
+}
