f0df2031a06ff67e1e7eb86e6c556cfe46e6a5c8
kev <yqlbu@bu.edu>
2024-03-31 22:14:09 +0800

    refactor(system): rework users module

---
+++ system/users/basic.nix
@@ -0,0 +1,23 @@
+{ inputs, pkgs, user, lib, ... }:
+
+with lib;
+let
+  keyFiles = [
+    "${inputs.home-estate}/authorized_keys"
+  ];
+in
+{
+  users.users = {
+    ${user} = {
+      isNormalUser = true;
+      extraGroups = mkDefault [ "networkmanager" "wheel" "docker" "libvirtd" ];
+      home = "/home/${user}";
+      # /etc/ssh/authorized_keys.d/${user}
+      openssh.authorizedKeys.keyFiles = keyFiles;
+    };
+
+    root = {
+      shell = pkgs.bash;
+    };
+  };
+}
