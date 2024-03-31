d8cefde8d8f19f7fc809bbbadd03cd18bf328aa7
kev <31861128+miooochi@users.noreply.github.com>
2024-04-01 00:32:05 +0800

    feat(system/services): add atticd (#189)

--- shared/server/home/packages.nix
+++ shared/server/home/packages.nix
@@ -3,26 +3,20 @@
 {
   home.packages = with pkgs; [
     # maintenance essentials
-    bat
     delta
-    jq
     lazygit
     neovim
     zoxide
     trash-cli
-    unzip
-    zip
 
     # monitoring
     btop
-    htop
     ncdu # disk utilization
     duf # disk usage analyzer
 
     # terminal
     fd
     fzf
-    tailspin # A log file highlighter
 
     # fish plugins
     fishPlugins.fzf-fish
---
+++ shared/server/secrets/attic-server.nix
@@ -0,0 +1,7 @@
+# References: https://colmena.cli.rs/unstable/features/keys.html
+{
+  "env" = {
+    keyFile = /run/secrets/attic/env;
+    destDir = "/etc/atticd";
+  };
+}
