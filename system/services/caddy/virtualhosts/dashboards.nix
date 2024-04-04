e93d18b70ebd8079a1741cfdef349669c181649b
kev <yqlbu@bu.edu>
2024-04-04 12:51:27 +0800

    feat(system/services/caddy): add genVirtualHosts func to simply management

--- system/services/caddy/virtualhosts/dashboards.nix
+++ system/services/caddy/virtualhosts/dashboards.nix
@@ -1,13 +1,36 @@
-_:
+{ lib, ... }:
 
 # References:
 # https://nixos.wiki/wiki/Caddy
+let
+  genVirtualHost = hosts: (
+    # (lib.attrsets.mergeAttrsList): merge attribute sets, expect input as a list
+    lib.attrsets.mergeAttrsList (map
+      (host: {
+        "${host.fqdn}".extraConfig = ''
+          ${(import (../common.nix)).hostCommonConfig}
+          reverse_proxy ${host.backend}
+        '';
+      })
+      hosts)
+  );
+in
 {
-  services.caddy = {
-    # Dashboard
-    virtualHosts."minio.homelab.local".extraConfig = ''
-      ${(import (../common.nix)).hostCommonConfig}
-      reverse_proxy http://10.178.0.81:9090
-    '';
-  };
+  # Dashboard
+  services.caddy.virtualHosts = genVirtualHost [
+    { fqdn = "minio.homelab.local"; backend = "http://10.178.0.81:9090"; }
+    { fqdn = "emby.homelab.local"; backend = "http://10.118.20.120:8096"; }
+    { fqdn = "vaultwarden.homelab.local"; backend = "http://10.178.0.161:8080"; }
+    { fqdn = "portainer.homelab.local"; backend = "http://10.178.0.81:9999"; }
+    { fqdn = "qbit.homelab.local"; backend = "http://10.178.0.81:6363"; }
+    { fqdn = "ros.homelab.local"; backend = "http://10.0.0.1:80"; }
+    { fqdn = "rss.homelab.local"; backend = "http://10.118.20.50:5880"; }
+    { fqdn = "semaphore.homelab.local"; backend = "http://10.178.0.40:3000"; }
+    { fqdn = "qnap.homelab.local"; backend = "https://10.178.0.81:5001"; }
+    { fqdn = "tmm.homelab.local"; backend = "http://10.118.20.122:5800"; }
+    { fqdn = "unifi.homelab.local"; backend = "https://10.178.0.16:8443"; }
+    { fqdn = "uptime.homelab.local"; backend = "http://10.118.20.47:3001"; }
+    { fqdn = "notestation.homelab.local"; backend = "http://10.178.0.85:9400"; }
+    { fqdn = "qumagie.homelab.local"; backend = "http://10.178.0.81:5000"; }
+  ];
 }
--- system/services/caddy/virtualhosts/datastore.nix
+++ system/services/caddy/virtualhosts/datastore.nix
@@ -1,13 +1,23 @@
-_:
+{ lib, ... }:
 
 # References:
 # https://nixos.wiki/wiki/Caddy
+let
+  genVirtualHost = hosts: (
+    # (lib.attrsets.mergeAttrsList): merge attribute sets, expect input as a list
+    lib.attrsets.mergeAttrsList (map
+      (host: {
+        "${host.fqdn}".extraConfig = ''
+          encode zstd gzip
+          reverse_proxy ${host.backend}
+        '';
+      })
+      hosts)
+  );
+in
 {
-  services.caddy = {
-    # Datastore
-    virtualHosts."s3.homelab.local".extraConfig = ''
-      encode zstd gzip
-      reverse_proxy http://10.178.0.81:9000
-    '';
-  };
+  # Datastore
+  services.caddy.virtualHosts = genVirtualHost [
+    { fqdn = "s3.homelab.local"; backend = "http://10.178.0.81:9000"; }
+  ];
 }
