5b0174f5f887a9d19579ce68480799800deacbb9
kev <yqlbu@bu.edu>
2024-04-05 23:06:53 +0800

    patch(home/pkgs): remove matrix client

--- home/packages/windowmanager/hyprland/im/default.nix
+++ home/packages/windowmanager/hyprland/im/default.nix
@@ -3,7 +3,6 @@
 {
   home.packages = with pkgs; [
     inputs.chaotic.packages.${system}.telegram-desktop_git
-    cinny-desktop # Yet another matrix client for desktop
     webcord # A Discord and SpaceBar electron-based client implemented without Discord API
   ];
 }
