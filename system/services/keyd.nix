3e14acd15089c6d2bc0caef2c7301f7a508429c3
kev <31861128+miooochi@users.noreply.github.com>
2024-04-13 21:37:48 +0800

    feat(system/services): add keyd (#207)

---
+++ system/services/keyd.nix
@@ -0,0 +1,12 @@
+_:
+
+# A key remapping daemon for linux.
+# Reference: https://github.com/rvaiya/keyd
+{
+  keyd = {
+    enable = true;
+    keyboards.default.settings = {
+      main = { };
+    };
+  };
+}
