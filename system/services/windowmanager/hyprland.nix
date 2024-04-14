ff6108731e60144defc6c8b35a638c1687d3c53e
kev <yqlbu@bu.edu>
2024-04-15 07:33:23 +0800

    chore(flake): update inputs.hyprland (v0.39.1)

--- system/services/windowmanager/hyprland.nix
+++ system/services/windowmanager/hyprland.nix
@@ -1,12 +1,23 @@
-{ inputs, pkgs, ... }:
+{ inputs, system, pkgs, ... }:
 
+# Reference:
+# https://wiki.hyprland.org/Nix/Hyprland-on-NixOS/
+let
+  hypr-pkgs = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+in
 {
   # enable hyprland
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    # xwayland = {
-    #   enable = true;
-    # };
+    package = inputs.hyprland.packages.${system}.hyprland;
+  };
+
+  # misc
+  hardware.opengl = {
+    package = hypr-pkgs.mesa.drivers;
+
+    # if you also want 32-bit support (e.g for Steam)
+    driSupport32Bit = true;
+    package32 = hypr-pkgs.pkgsi686Linux.mesa.drivers;
   };
 }
