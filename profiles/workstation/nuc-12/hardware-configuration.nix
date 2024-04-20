5c7eb1147c5c507419c06c55935c4b427de2f318
kev <yqlbu@bu.edu>
2024-04-20 15:14:37 +0800

    patch(profiles/workstation): update kmodule list

--- profiles/workstation/nuc-12/hardware-configuration.nix
+++ profiles/workstation/nuc-12/hardware-configuration.nix
@@ -23,7 +23,7 @@
 
     initrd = {
       availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" "lz4" ];
-      kernelModules = [ "xe" "i2c_dev" "coretemp" "kvm-intel" ];
+      kernelModules = [ "xe" "i2c_dev" "coretemp" ];
       luks.devices."root" = {
         device = "/dev/disk/by-uuid/6cf8230d-0ddf-44b2-9be0-c24ce47072e3";
         # the keyfile(or device partition) that should be used as the decryption key for the encrypted device.
