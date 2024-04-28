c2e9d83c9123997817b1de50a5f67c31328097ca
kev <yqlbu@bu.edu>
2024-04-28 17:56:24 +0800

    patch(services/encryption): remove bitwarden-cli

--- home/services/encryption/default.nix
+++ home/services/encryption/default.nix
@@ -12,7 +12,6 @@
       age-plugin-yubikey # YubiKey plugin for age
       yubikey-manager # Command line tool for configuring any YubiKey over all USB transports
       yubioath-flutter # Yubico Authenticator for Desktop
-      bitwarden-cli # A secure and free password manager for all of your devices (CLI)
       bitwarden # A secure and free password manager for all of your devices (UI)
     ];
   };
