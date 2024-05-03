25f1f104a6a40e9796e52914f90508b22a4773a1
kev <yqlbu@bu.edu>
2024-05-03 21:25:20 +0800

    patch(system/security): remove redundant line

--- system/security/polkit.nix
+++ system/security/polkit.nix
@@ -3,10 +3,10 @@
 {
 
   environment.systemPackages = with pkgs; [
-    polkit # policy daemon
     polkit-kde-agent
   ];
 
+  # policy daemon
   security.polkit = {
     enable = true;
     debug = true;
