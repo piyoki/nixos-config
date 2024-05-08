b1878eff6a2518f290d663ac01c2e60f742cb8ec
kev <yqlbu@bu.edu>
2024-05-08 22:04:57 +0800

    feat(home/apps/mpv): add uosc support

--- home/apps/mpv/default.nix
+++ home/apps/mpv/default.nix
@@ -1,6 +1,18 @@
-_:
+{ pkgs, ... }:
 
+# References:
+# https://mpv.io/manual/stable/
+# https://wiki.archlinux.org/title/mpv
+# https://github.com/tomasklaen/uosc
+let
+  scriptSrc = pkgs.mpvScripts.uosc;
+in
 {
-  xdg.configFile."mpv/mpv.conf".source = ./mpv.conf;
-}
+  home.packages = [ scriptSrc ];
 
+  xdg.configFile = {
+    "mpv/mpv.conf".source = ./mpv.conf;
+    "mpv/fonts".source = scriptSrc + "/share/fonts/";
+    "mpv/scripts".source = scriptSrc + "/share/mpv/scripts/";
+  };
+}
