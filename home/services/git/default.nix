e470322b02019531fc9763a47b155b6ba216b2f6
kev <yqlbu@bu.edu>
2024-06-15 13:07:24 +0800

    patch(home/services/git): add references; update default config

--- home/services/git/default.nix
+++ home/services/git/default.nix
@@ -1,5 +1,12 @@
 _:
 
+# References:
+# https://docs.github.com/en/authentication/troubleshooting-ssh/using-ssh-over-the-https-port
+# https://docs.github.com/en/account-and-profile/setting-up-and-managing-your-personal-account-on-github/managing-your-personal-account/managing-multiple-accounts
+# https://blog.gitguardian.com/8-easy-steps-to-set-up-multiple-git-accounts/
+
+# Contributing to multiple accounts using SSH and GIT_SSH_COMMAND
+# GIT_SSH_COMMAND='ssh -i ~/.ssh/<private-key> -o IdentitiesOnly=yes' git clone git@github.com:<username>/<repo>.git
 {
   home.file.".gitconfig".source = ./gitconfig;
 }
