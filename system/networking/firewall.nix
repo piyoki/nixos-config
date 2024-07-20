ddfbcd433b165a2d706f54d9d8bcac43dc1a0d08
kev <31861128+piyoki@users.noreply.github.com>
2024-07-19 20:39:07 -0400

    patch(system/networking/firewall): open tailscale udp port

--- system/networking/firewall.nix
+++ system/networking/firewall.nix
@@ -5,12 +5,15 @@ let
     22 # ssh
     53317 # localsend
   ];
+  specialUDPPorts = [
+    41641 # tailscale for peer-to-peer connection
+  ];
 in
 {
   networking.firewall = {
     enable = true;
     # open ports in firewall
     allowedTCPPorts = commonPorts;
-    allowedUDPPorts = commonPorts;
+    allowedUDPPorts = commonPorts ++ specialUDPPorts;
   };
 }
