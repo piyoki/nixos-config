09e71b21b8885e89bffd6259c14c8e16b2be444b
kev <31861128+piyoki@users.noreply.github.com>
2024-07-22 06:29:46 -0400

    fix: fix compilation conflicts

--- system/internationalisation/fcitx.nix
+++ system/internationalisation/fcitx.nix
@@ -2,8 +2,8 @@
 
 {
   i18n.inputMethod = {
-    type = "fcitx5";
-    enabled = true;
+    # type = "fcitx5";
+    enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-gtk
       fcitx5-chinese-addons
