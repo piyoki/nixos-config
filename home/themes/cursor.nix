8cc0c129afa87f0427ad956a2eccf48a5245d5c6
kev <31861128+piyoki@users.noreply.github.com>
2024-07-23 23:56:46 -0400

    patch(home/themes/cursor): update default cursor size from 24 to 52

--- home/themes/cursor.nix
+++ home/themes/cursor.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }:
 
+# Ref: https://github.com/hyprwm/Hyprland/issues/1577
+# Command to set:
+# hyprctl setcursor Bibata-Modern-Ice 46
 {
   home.pointerCursor = {
     gtk.enable = true;
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
-    size = 24;
+    size = 46; #24
   };
 }
