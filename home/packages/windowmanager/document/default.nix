88ad0455f58586d3a1146e6b671d3084748a1715
kev <31861128+piyoki@users.noreply.github.com>
2024-08-04 11:52:02 -0400

    feat(home/pkgs/document): add system-config-printer

--- home/packages/windowmanager/document/default.nix
+++ home/packages/windowmanager/document/default.nix
@@ -4,6 +4,7 @@
   home.packages = with pkgs; [
     gwenview # image viewer
     okular # document viewer
+    system-config-printer # gtk printer interface
     libreoffice-fresh # office softwares
     poppler_utils # pdf utils (CLI), pdfunite included
     ghostscript # PostScript interpreter (mainline version)
