9b977a6a3317bbe6c0812f0854a19ebb8290cb60
kev <31861128+piyoki@users.noreply.github.com>
2024-08-12 09:54:07 -0400

    patch(home/browser/firefox): use stable firefox; enable kde-file-picker

--- home/packages/windowmanager/browser/firefox.nix
+++ home/packages/windowmanager/browser/firefox.nix
@@ -1,4 +1,4 @@
-{ inputs, system, ... }:
+{ pkgs, ... }:
 
 # References:
 # https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265/23
@@ -8,11 +8,12 @@ in
 {
   programs.firefox = {
     enable = true;
-    package = inputs.chaotic.packages.${system}.firefox_nightly;
+    package = (pkgs.wrapFirefox (pkgs.firefox-unwrapped.override { pipewireSupport = true; }) { });
     policies = {
       /* ---- PREFERENCES ---- */
       # Set preferences shared by all profiles.
       Preferences = {
+        "widget.use-xdg-desktop-portal.file-picker" = { Value = true; } // lock;
         "browser.contentblocking.category" = { Value = "strict"; } // lock;
         "extensions.pocket.enabled" = { Value = true; } // lock;
         "extensions.screenshots.disabled" = { Value = false; } // lock;
