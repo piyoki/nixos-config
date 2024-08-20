0ce6fe396668da221deaf6dc41db964603d67f4e
kev <31861128+piyoki@users.noreply.github.com>
2024-08-19 20:44:23 -0400

    patch(system/svc): rework scx configuration; demise legacy configs

--- system/services/scx.nix
+++ system/services/scx.nix
@@ -15,29 +15,37 @@
 # 21:38:53 [INFO] DOM[01] cpumask 0000000000FC0FC0 (12 cpus)
 # 21:38:53 [INFO] Rusty Scheduler Attached
 let
-  bin = inputs.chaotic.packages.${system}.scx;
+  scx_bin = inputs.chaotic.packages.${system}.scx;
 in
 {
-
+  # --- Legacy Configuration ---
   # A Linux kernel feature which enables implementing kernel thread schedulers in BPF and dynamically loading them.
-  environment.systemPackages = [ bin ];
+  # environment.systemPackages = [ bin ];
 
   # systemd unit
   # /etc/systemd/system/scx.service
-  systemd.services."scx" = {
-    description = "Start scx_scheduler";
-    documentation = [ "https://github.com/sched-ext/scx" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.writeShellScript "scx" ''
-        set -eux
-        ${bin}/bin/scx_bpfland
-      ''}";
-      Type = "simple";
-      StandardError = "journal";
-      LogNamespace = "sched-ext";
-      Restart = "always";
-      Environment = [ ];
-    };
-    wantedBy = [ "multi-user.target" ];
+  # systemd.services."scx" = {
+  #   description = "Start scx_scheduler";
+  #   documentation = [ "https://github.com/sched-ext/scx" ];
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.writeShellScript "scx" ''
+  #       set -eux
+  #       ${bin}/bin/scx_bpfland
+  #     ''}";
+  #     Type = "simple";
+  #     StandardError = "journal";
+  #     LogNamespace = "sched-ext";
+  #     Restart = "always";
+  #     Environment = [ ];
+  #   };
+  #   wantedBy = [ "multi-user.target" ];
+  # };
+
+  # --- New Configuration ---
+  chaotic.scx = {
+    enable = true;
+    package = scx_bin;
+    # one of "scx_bpfland", "scx_central", "scx_flatcg", "scx_lavd", "scx_layered", "scx_nest", "scx_pair", "scx_qmap", "scx_rlfifo", "scx_rustland", "scx_rusty", "scx_simple", "scx_userland"
+    scheduler = "scx_bpfland";
   };
 }
