ddfc59578ac501d2aee00efde917b4e008bc63ff
kev <31861128+piyoki@users.noreply.github.com>
2024-08-23 16:26:21 -0400

    patch(system/services/flatpak): rework flatpak service

--- system/services/flatpak.nix
+++ system/services/flatpak.nix
@@ -1,12 +1,12 @@
-{ pkgs, user, ... }:
+# Reference:
+# https://nixos.wiki/wiki/Flatpak
 
-# Reference: https://nixos.wiki/wiki/Flatpak
+# Usage:
+# flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# flatpak update
+# flatpak search bustle
+# flatpak install flathub org.freedesktop.Bustle
+# flatpak run org.freedesktop.Bustle
 {
   services.flatpak.enable = true;
-
-  users = {
-    users.${user} = {
-      packages = with pkgs; [ flatpak ];
-    };
-  };
 }
