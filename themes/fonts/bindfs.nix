9d56d34488f90fa114ea4c32666e78099052e746
kev <31861128+piyoki@users.noreply.github.com>
2024-08-28 22:14:14 -0400

    feat(themes/fonts): add bindfs integration (#301)

---
+++ themes/fonts/bindfs.nix
@@ -0,0 +1,33 @@
+# Reference: https://github.com/NixOS/nixpkgs/issues/119433
+# bindfs resolves all symlink, allowing them to be accessed without letting /nix into the sandbox.
+
+{ config, pkgs, ... }:
+
+{
+  system.fsPackages = [ pkgs.bindfs ];
+  fileSystems =
+    let
+      mkRoSymBind = path: {
+        device = path;
+        fsType = "fuse.bindfs";
+        options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+      };
+      aggregatedIcons = pkgs.buildEnv {
+        name = "system-icons";
+        paths = with pkgs; [
+          #libsForQt5.breeze-qt5  # for plasma
+          gnome.gnome-themes-extra
+        ];
+        pathsToLink = [ "/share/icons" ];
+      };
+      aggregatedFonts = pkgs.buildEnv {
+        name = "system-fonts";
+        paths = config.fonts.packages;
+        pathsToLink = [ "/share/fonts" ];
+      };
+    in
+    {
+      "/usr/share/icons" = mkRoSymBind "${aggregatedIcons}/share/icons";
+      "/usr/local/share/fonts" = mkRoSymBind "${aggregatedFonts}/share/fonts";
+    };
+}
