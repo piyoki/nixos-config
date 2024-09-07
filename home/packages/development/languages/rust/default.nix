3fdd406d806d80cd11345c79c0fb19f004c50641
kev <31861128+piyoki@users.noreply.github.com>
2024-09-06 21:49:51 -0400

    refactor(home/pkgs/development): rework development module

--- home/packages/development/languages/rust/default.nix
+++ home/packages/development/languages/rust/default.nix
@@ -2,8 +2,8 @@
 
 {
   home.packages = with pkgs; [
-    rustc
-    cargo
+    rustc # Safe, concurrent, practical language (wrapper script)
+    cargo # Downloads your Rust project's dependencies and builds your project
+    rustfmt # Tool for formatting Rust code according to style guidelines
   ];
 }
-
