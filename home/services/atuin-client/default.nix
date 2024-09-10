7c6188fb6340699666e1d99e0f45790e596d70b4
kev <31861128+piyoki@users.noreply.github.com>
2024-09-09 23:14:09 -0400

    patch(home/svc/atuin-client): reable atuin-client

--- home/services/atuin-client/default.nix
+++ home/services/atuin-client/default.nix
@@ -2,12 +2,12 @@ _:
 
 {
   programs.atuin = {
-    enable = false;
-    # settings = {
-    #   auto_sync = true;
-    #   update_check = true;
-    #   sync_address = "http://10.118.25.31:8888";
-    #   sync_frequency = "10m";
-    # };
+    enable = true;
+    settings = {
+      auto_sync = true;
+      update_check = true;
+      sync_address = "http://10.118.25.31:8888";
+      sync_frequency = "10m";
+    };
   };
 }
