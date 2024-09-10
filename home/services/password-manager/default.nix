c8ea61060793d163a2313fc8ec6f2b6905bd4250
kev <31861128+piyoki@users.noreply.github.com>
2024-09-09 23:15:33 -0400

    refactor(home/svc): rework password-manager(encryption) module (#320)
    * patch(home/svc/atuin-client): disable atuin
    
    * refactor(shared/server/system/svc): remove sops module (not needed)
    
    * refactor(home/svc/encryption): demise server module (not needed)
    
    * refactor(home/svc): rework password-manager(encryption) module
    
    * patch(home/svc/atuin-client): reable atuin-client
    
    (cherry picked from commit 05e876d8de509ba85eea5f80eb656c6772df2836)

---
+++ home/services/password-manager/default.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./keybase.nix
+    ./sops.nix
+  ];
+
+  home.packages = with pkgs; [
+    gopass # The slightly more awesome Standard Unix Password Manager for Teams. Written in Go
+    age-plugin-yubikey # YubiKey plugin for age
+    yubikey-manager # Command line tool for configuring any YubiKey over all USB transports
+    yubioath-flutter # Yubico Authenticator for Desktop
+    bitwarden # A secure and free password manager for all of your devices (UI)
+  ];
+
+}
