8bb5e775135af449c11fdbcf1467a1e068a023ea
kev <31861128+piyoki@users.noreply.github.com>
2024-09-14 08:19:20 -0400

    feat(shared/nixos): add nixos cache public-key

--- shared/nixos.nix
+++ shared/nixos.nix
@@ -23,6 +23,7 @@
         "https://nyx.chaotic.cx/"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       ];
