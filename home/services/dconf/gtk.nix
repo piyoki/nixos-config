65bb581be8b6099c3915a21604317952cc82fe07
kev <31861128+piyoki@users.noreply.github.com>
2024-09-17 21:55:00 -0400

    refactor(home/svc/dconf): separate dconf configuration modules

---
+++ home/services/dconf/gtk.nix
@@ -0,0 +1,12 @@
+_:
+
+{
+  # dconf configuration options
+  dconf.settings = {
+    # gtk specific
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      default-web-browser = "org.qutebrowser.qutebrowser.desktop";
+    };
+  };
+}
