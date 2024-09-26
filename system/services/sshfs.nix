f776df3b772fa3d657b1c3959ac0dee3b64f5770
kev <31861128+piyoki@users.noreply.github.com>
2024-09-26 09:54:50 -0400

    chore(system/svc/sshfs): add addtional references

--- system/services/sshfs.nix
+++ system/services/sshfs.nix
@@ -2,6 +2,7 @@
 
 # Reference:
 # https://wiki.nixos.org/wiki/SSHFS
+# https://unix.stackexchange.com/questions/94720/connection-reset-by-peer-using-sshfs
 
 # Quick connection command:
 # sshfs -o debug,sshfs_debug,loglevel=debug,allow_other,default_permissions <user>@<host>:<remote_mount_path> -p <port> <local_mount_path>
