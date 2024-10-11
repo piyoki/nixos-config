3b84b4b91aee7ae76ad4332ca2dda8a9906c81ae
kev <31861128+piyoki@users.noreply.github.com>
2024-10-11 09:56:30 -0400

    feat(home/assets): create default avatar file to $HOME/.face

--- home/assets/avatars.nix
+++ home/assets/avatars.nix
@@ -2,4 +2,5 @@
 
 {
   home.file."Pictures/Avatars".source = inputs.assets.packages.${system}.wallpapers + "/share/avatars";
+  home.file.".face".source = inputs.assets.packages.${system}.avatars + "/share/avatars/default.jpg";
 }
