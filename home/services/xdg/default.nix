1adafc2f400b3b85714cacc04c2c7602451f51cc
kev <31861128+piyoki@users.noreply.github.com>
2024-10-11 10:01:18 -0400

    feat(home/assets): add avatars module (#357)
    * chore(flake): update inputs.assets
    
    * feat(home/assets): add avatars module
    
    * refactor(shared/modules/tmpfs): remove $HOME/.face
    
    * feat(home/svc/xdg): add $XDG_AVATAR_DIR
    
    * feat(home/assets): create default avatar file to $HOME/.face

--- home/services/xdg/default.nix
+++ home/services/xdg/default.nix
@@ -80,6 +80,7 @@
       extraConfig = {
         XDG_SCREENSHOTS_DIR = "${config.xdg.userDirs.pictures}/Screenshots";
         XDG_WALLPAPER_DIR = "${config.xdg.userDirs.pictures}/Wallpapers";
+        XDG_AVATAR_DIR = "${config.xdg.userDirs.pictures}/Avatars";
         XDG_MEDIA_DIR = "${config.home.homeDirectory}/Media";
         XDG_TANK_DIR = "${config.home.homeDirectory}/Tank";
         XDG_SHARED_DIR = "${config.home.homeDirectory}/Shared";
