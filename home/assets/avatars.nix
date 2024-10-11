bcbbb1ed5ce50617f842f454153ef1dc58c28db0
kev <31861128+piyoki@users.noreply.github.com>
2024-10-11 10:02:18 -0400

    fix(home/assets/avatars): fix broken link

--- home/assets/avatars.nix
+++ home/assets/avatars.nix
@@ -1,6 +1,6 @@
 { inputs, system, ... }:
 
 {
-  home.file."Pictures/Avatars".source = inputs.assets.packages.${system}.wallpapers + "/share/avatars";
+  home.file."Pictures/Avatars".source = inputs.assets.packages.${system}.avatars + "/share/avatars";
   home.file.".face".source = inputs.assets.packages.${system}.avatars + "/share/avatars/default.jpg";
 }
