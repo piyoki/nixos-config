cb2409ebb2ffa596230ee2a980862d42f9b04d87
kev <31861128+piyoki@users.noreply.github.com>
2024-10-13 10:05:35 -0400

    refactor(system/enviromment): rework environment module

--- system/environment/default.nix
+++ system/environment/default.nix
@@ -1,24 +1,8 @@
 _:
 
 {
-  # Environment vars
-  environment = {
-    # system-level variables
-    variables = {
-      # set vim as the default editor
-      EDITOR = "vim";
-    };
-
-    # session-specfic variables
-    sessionVariables = {
-      # Nix specific
-      NIX_SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
-      # Wayland specific
-      NIXOS_OZONE_WL = "1";
-      # Default applications
-      BROWSER = "qutebrowser";
-      # Force intel-media-driver
-      LIBVA_DRIVER_NAME = "iHD";
-    };
-  };
+  imports = [
+    ./development.nix
+    ./system.nix
+  ];
 }
