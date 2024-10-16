066055622e255aa217e216b7fbb221dbdd5a4e84
kev <31861128+piyoki@users.noreply.github.com>
2024-10-16 00:34:04 -0400

    patch(home/apps/mpv): sync mpv configs from inputs.dotfiles

--- home/apps/mpv/default.nix
+++ home/apps/mpv/default.nix
@@ -1,18 +1,16 @@
-{ pkgs, ... }:
+{ inputs, system, ... }:
 
 # References:
 # https://mpv.io/manual/stable/
 # https://wiki.archlinux.org/title/mpv
 # https://github.com/tomasklaen/uosc
-let
-  scriptSrc = pkgs.mpvScripts.uosc;
-in
 {
-  home.packages = [ scriptSrc ];
+  xdg.configFile."mpv".source = inputs.dotfiles.packages.${system}.mpv-universal + "/";
+  # home.packages = [ scriptSrc ];
 
-  xdg.configFile = {
-    "mpv/mpv.conf".source = ./mpv.conf;
-    "mpv/fonts".source = scriptSrc + "/share/fonts/";
-    "mpv/scripts".source = scriptSrc + "/share/mpv/scripts/";
-  };
+  # xdg.configFile = {
+  #   "mpv/mpv.conf".source = ./mpv.conf;
+  #   "mpv/fonts".source = scriptSrc + "/share/fonts/";
+  #   "mpv/scripts".source = scriptSrc + "/share/mpv/scripts/";
+  # };
 }
