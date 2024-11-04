c7505fa29e77dcaa003aed367c71bcf0672f7ab4
kev <31861128+piyoki@users.noreply.github.com>
2024-11-04 11:17:56 -0500

    patch(home/svc/password-manager): use nixpkgs-small as upstream for yubikey packages

--- home/services/password-manager/yubikey.nix
+++ home/services/password-manager/yubikey.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-small, ... }:
 
 # Reference: https://nixos.wiki/wiki/Yubikey
 {
-  home.packages = with pkgs; [
-    age-plugin-yubikey # YubiKey plugin for age
-    yubikey-manager # Command line tool for configuring any YubiKey over all USB transports
-    yubioath-flutter # Yubico Authenticator for Desktop
+  home.packages = [
+    pkgs.age-plugin-yubikey # YubiKey plugin for age
+    pkgs-small.yubikey-manager # Command line tool for configuring any YubiKey over all USB transports
+    pkgs-small.yubioath-flutter # Yubico Authenticator for Desktop
   ];
 }
