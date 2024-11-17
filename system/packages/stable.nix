317513a669a389d2a8fa0dc279c8f5b6cd9aa5b5
kev <31861128+piyoki@users.noreply.github.com>
2024-11-16 23:49:30 -0500

    patch(system): apply a few system patches; update pkgs.yubikey-desktop  (#383)
    * patch(shared/nixos): update gc cron from 3d to 14d
    
    * patch(home/pkgs): remove cava
    
    * patch(home/pkg/firefox): switch to firefox-nightly
    
    * refactor(system/pkg): migrate tspin
    
    * docs(bootstrap-system): update default params
    
    * chore(flake): update inputs.{nixpkgs,home-manager}
    
    * refactor(home/svc/password-manager): migrate yubikey-desktop

--- system/packages/stable.nix
+++ system/packages/stable.nix
@@ -1,9 +1,5 @@
 { pkgs-stable, ... }:
 
 {
-  # ebpf
-  environment.systemPackages = with pkgs-stable; [
-    tailspin # A log file highlighter
-    pkgs-small.yubioath-flutter # Yubico Authenticator for Desktop
-  ];
+  environment.systemPackages = with pkgs-stable; [ ];
 }
