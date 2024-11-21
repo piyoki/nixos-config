26ac655441c87cf0df23db3864418f78062ec6b8
kev <31861128+piyoki@users.noreply.github.com>
2024-11-21 10:02:43 -0500

    refactor(home/pkg): tidy up packages

--- home/packages/stable/default.nix
+++ home/packages/stable/default.nix
@@ -2,6 +2,10 @@
 
 {
   home.packages = with pkgs-stable; [
+    # media
+    cava # for visualizing audio
+
+    # productivity
     mountpoint-s3 # A simple, high-throughput file client for mounting an Amazon S3 bucket as a local file system.
   ];
 }
--- home/services/password-manager/yubikey.nix
+++ home/services/password-manager/yubikey.nix
@@ -1,9 +1,10 @@
-{ pkgs, pkgs-small, ... }:
+{ pkgs, ... }:
 
 # Reference: https://nixos.wiki/wiki/Yubikey
 {
-  home.packages = [
-    pkgs.age-plugin-yubikey # YubiKey plugin for age
-    pkgs-small.yubikey-manager # Command line tool for configuring any YubiKey over all USB transports
+  home.packages = with pkgs; [
+    age-plugin-yubikey # YubiKey plugin for age
+    yubikey-manager # Command line tool for configuring any YubiKey over all USB transports
+    yubioath-flutter # Yubico Authenticator for Desktop
   ];
 }
