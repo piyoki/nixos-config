99e6025045148aa13eba7044a187a647947a51f0
kev <31861128+piyoki@users.noreply.github.com>
2024-12-08 09:39:34 -0500

    feat(home/pkgs/programming/rust): add uv

--- home/packages/development/language/rust/default.nix
+++ home/packages/development/language/rust/default.nix
@@ -1,10 +1,11 @@
-{ inputs, system, ... }:
+{ pkgs, inputs, system, ... }:
 
 # References:
 # https://github.com/nix-community/fenix
 
 {
-  home.packages = with inputs.rust-nightly-overlay.packages.${system}.latest; [
-    toolchain # derivation with all the Rust components; including rust-analyzer (LSP)
+  home.packages = [
+    inputs.rust-nightly-overlay.packages.${system}.latest.toolchain # derivation with all the Rust components; including rust-analyzer (LSP)
+    pkgs.uv # Extremely fast Python package installer and resolver, written in Rust
   ];
 }
