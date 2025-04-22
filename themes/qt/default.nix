0d97385de0583d1e4fe9b54e022887dab61061d0
kev <31861128+piyoki@users.noreply.github.com>
2025-04-21 20:42:58 -0400

    patch(themes/qt): update default theme configs

--- themes/qt/default.nix
+++ themes/qt/default.nix
@@ -1,14 +1,7 @@
-{ pkgs, ... }:
-
 {
-  nixpkgs = {
-    config.qt = {
-      enable = true;
-      platformTheme = "qt5ct";
-      style = {
-        package = with pkgs; [ adwaita-qt adwaita-qt6 gnome.adwaita-icon-theme ];
-        name = "adwaita-dark";
-      };
-    };
+  config.qt = {
+    enable = true;
+    platformTheme = "qt5ct";
+    style = "adwaita-dark";
   };
 }
