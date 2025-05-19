c60b35d8dc002c7ead0b9cbc9c54a92584fd882a
kev <31861128+piyoki@users.noreply.github.com>
2025-05-19 18:59:16 -0400

    chore(home/document): add pdf utils usage

--- home/packages/windowmanager/document/default.nix
+++ home/packages/windowmanager/document/default.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }:
 
+# -- Advanced PDF utilities Usage --
+# convert pdf to image by poppler-utils
+# pdftoppm input.pdf output -jpeg
+# combine multiple pdfs into one by poppler-utils
+# pdfunite input1.pdf input2.pdf output.pdf
+# decrypt pdf by qpdf
+# qpdf --decrypt --password=PASSWORD input.pdf output.pdf
+
 {
   home.packages = with pkgs; [
     kdePackages.gwenview # image viewer
