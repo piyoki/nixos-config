4557fdfce408ae008638556711c9f6394880294b
kev <31861128+piyoki@users.noreply.github.com>
2025-05-28 19:35:34 -0400

    feat(system/svc): add ttyd (#457)

--- system/services/default.nix
+++ system/services/default.nix
@@ -5,19 +5,20 @@ _:
     # ./cifs-mount.nix
     # ./displaymanager.nix
     # ./docker.nix
-    ./podman.nix
-    ./greetd.nix
+    # ./xdg-portal.nix
     ./fish.nix
     ./flatpak.nix
     ./gnome-keyring.nix
     ./gnupg
+    ./greetd.nix
     ./openssh
+    ./podman.nix
     ./powermanagement
     ./printer.nix
     ./rclone.nix
     ./thunar.nix
+    ./ttyd.nix
     ./windowmanager
-    # ./xdg-portal.nix
     ./zramd.nix
   ];
 }
