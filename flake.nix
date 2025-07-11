3794a09b7bd52e031188856db59ccd0c9072ee1c
kev <31861128+piyoki@users.noreply.github.com>
2025-07-11 09:01:25 -0400

    chore(flake): update inputs.pyprland (2.4.6)

--- flake.nix
+++ flake.nix
@@ -136,7 +136,7 @@
     sops-nix.url = "github:Mic92/sops-nix";
     nixpkgs-wayland = { url = "github:nix-community/nixpkgs-wayland"; inputs.nixpkgs.follows = "nixpkgs"; };
     hyprland = { url = "git+https://github.com/hyprwm/Hyprland?submodules=1&ref=refs/tags/v0.49.0"; };
-    pyprland = { url = "github:hyprland-community/pyprland/cc1be63ef4b998f4aebd5336a148b0aa267b54a7"; };
+    pyprland = { url = "git+https://github.com/hyprland-community/pyprland?ref=refs/tags/2.4.6"; };
     rust-nightly-overlay = { url = "github:nix-community/fenix"; inputs.nixpkgs.follows = "nixpkgs"; };
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     impermanence.url = "github:nix-community/impermanence";
