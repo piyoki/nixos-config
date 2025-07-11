56a000c319c555075d557fe0509e15a0e704c1b5
kev <31861128+piyoki@users.noreply.github.com>
2025-07-11 08:52:23 -0400

    patch(system/environment/system): add wayland sepecific sessionVariables

--- system/environment/system.nix
+++ system/environment/system.nix
@@ -13,13 +13,19 @@ _:
     sessionVariables = {
       # Nix specific
       NIX_SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
-      # Wayland specific
-      NIXOS_OZONE_WL = "1";
       # Default applications
       BROWSER = "firefox";
       # Fix Wayland VRR issues since kernel 6.11.0+
       # Ref: https://gitlab.freedesktop.org/drm/amd/-/issues/2186
       WLR_DRM_NO_ATOMIC = 1;
+      # Wayland specific
+      MOZ_ENABLE_WAYLAND = 1;
+      CLUTTER_BACKEND = "wayland";
+      XDG_SESSION_TYPE = "wayland";
+      QT_QPA_PLATFORM = "wayland";
+      GDK_BACKEND = "wayland";
+      NIXOS_OZONE_WL = 1;
+      ELECTRON_OZONE_PLATFORM_HINT = "auto";
     };
   };
 }
