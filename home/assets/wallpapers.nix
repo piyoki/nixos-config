ced0b6cf0a30cac5cdf0552db70dabbbd99f5539
kev <31861128+piyoki@users.noreply.github.com>
2025-08-26 11:23:27 -0400

    feat(home/assets/wallpapers): add private wallpapers from inputs.home-estate

--- home/assets/wallpapers.nix
+++ home/assets/wallpapers.nix
@@ -1,5 +1,8 @@
 { inputs, system, ... }:
 
 {
-  home.file."Pictures/Wallpapers".source = inputs.assets.packages.${system}.wallpapers + "/share/wallpapers";
+  # Public wallpapers from nur-assets
+  home.file."Pictures/Wallpapers/Public".source = inputs.assets.packages.${system}.wallpapers + "/share/wallpapers";
+  # Private wallpapers from home-estate
+  home.file."Pictures/Wallpapers/Private/Chroma".source = inputs.home-estate.packages.${system}.chroma_wallpapers + "/share/chroma_wallpapers";
 }
--- home/services/xdg/default.nix
+++ home/services/xdg/default.nix
@@ -79,7 +79,8 @@
       # extra dirs
       extraConfig = {
         XDG_SCREENSHOTS_DIR = "${config.xdg.userDirs.pictures}/Screenshots";
-        XDG_WALLPAPER_DIR = "${config.xdg.userDirs.pictures}/Wallpapers";
+        XDG_WALLPAPER_PUBLIC_DIR = "${config.xdg.userDirs.pictures}/Wallpapers/Public";
+        XDG_WALLPAPER_PRIVATE_DIR = "${config.xdg.userDirs.pictures}/Wallpapers/Private";
         XDG_AVATAR_DIR = "${config.xdg.userDirs.pictures}/Avatars";
         XDG_MEDIA_DIR = "${config.home.homeDirectory}/Media";
         XDG_TANK_DIR = "${config.home.homeDirectory}/Tank";
