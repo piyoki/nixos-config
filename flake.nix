835b325e4f735dd5446bf592190ff98793fa40df
kev <31861128+piyoki@users.noreply.github.com>
2025-09-28 09:27:06 -0400

    chore(flake): update inputs.chaotic-kernel (6.16.9)

--- flake.nix
+++ flake.nix
@@ -131,7 +131,7 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     pilots.url = "github:NixOS-Pilots/pilots";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-    chaotic-kernel.url = "github:chaotic-cx/nyx?rev=c2bc079a4179295eee766d00b9ae264e68e13db9";
+    chaotic-kernel.url = "github:chaotic-cx/nyx?rev=c4928ddfaf2fa4375e6cee429ad7d0ebd61222fd";
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
     sops-nix.url = "github:Mic92/sops-nix";
     nixpkgs-wayland = { url = "github:nix-community/nixpkgs-wayland"; inputs.nixpkgs.follows = "nixpkgs"; };
