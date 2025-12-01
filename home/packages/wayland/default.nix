55775cdc9c02d0bfea518a6e95db50518459fd6c
kev <31861128+piyoki@users.noreply.github.com>
2025-11-30 22:01:56 -0500

    feat(home/pkgs/wayland): add wlr-randr

--- home/packages/wayland/default.nix
+++ home/packages/wayland/default.nix
@@ -1,12 +1,13 @@
-{ inputs, pkgs, ... }:
+{ inputs, system, ... }:
 
 {
-  home.packages = with pkgs; [
-    inputs.nixpkgs-wayland.packages.${system}.wdisplays # A graphical application for configuring displays in Wayland compositors
-    inputs.nixpkgs-wayland.packages.${system}.wl-clipboard # Command-line copy/paste utilities for Wayland
-    # inputs.nixpkgs-wayland.packages.${system}.xdg-desktop-portal-wlr # xdg-desktop-portal backend for wlroots
-    # inputs.nixpkgs-wayland.packages.${system}.wlroots # A modular Wayland compositor library
-    # inputs.nixpkgs-wayland.packages.${system}.new-wayland-protocols # Wayland protocol extensions
-    inputs.nixpkgs-wayland.packages.${system}.wev # Wayland event viewer
+  home.packages = with inputs.nixpkgs-wayland.packages.${system}; [
+    wdisplays # A graphical application for configuring displays in Wayland compositors
+    wl-clipboard # Command-line copy/paste utilities for Wayland
+    # xdg-desktop-portal-wlr # xdg-desktop-portal backend for wlroots
+    # wlroots # A modular Wayland compositor library
+    # new-wayland-protocols # Wayland protocol extensions
+    wev # Wayland event viewer
+    wlr-randr # A tool to configure outputs for wlroots-based Wayland compositors
   ];
 }
