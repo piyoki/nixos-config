06ad80711009e0c779ac5e67d4bb87b62a4e6e52
kev <31861128+piyoki@users.noreply.github.com>
2026-01-04 22:29:48 -0500

    patch(home/svc/wayland-session): update hyprland startup cmd

--- home/services/wayland-session/hyprland.nix
+++ home/services/wayland-session/hyprland.nix
@@ -5,7 +5,7 @@
   # NOTE: this executable is used by greetd to start a wayland session when system boot up
   # with such a vendor-no-locking script, we can switch to another wayland compositor without modifying greetd's config in NixOS module
   home.file.".wayland-session" = {
-    source = "${inputs.hyprland.packages.${system}.hyprland}/bin/Hyprland";
+    source = "${inputs.hyprland.packages.${system}.hyprland}/bin/start-hyprland";
     executable = true;
   };
 }
