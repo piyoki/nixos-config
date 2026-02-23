2ba373929ce41bfcf939f1f529c9b859d63b1ad7
kev <31861128+piyoki@users.noreply.github.com>
2026-02-22 22:49:16 -0500

    patch(home/pkg/development/golang): persist go env

--- home/packages/development/language/golang/default.nix
+++ home/packages/development/language/golang/default.nix
@@ -4,4 +4,10 @@
   home.packages = with pkgs; [
     golangci-lint # Fast linters Runner for Go
   ];
+
+  programs.go.env = {
+    GONOSUMDB = "gitea.hikarilab.me/*";
+    GOPRIVATE = "gitea.hikarilab.me/*";
+    GOPROXY = "direct";
+  };
 }
