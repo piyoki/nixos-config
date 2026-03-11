18887d37c5ec291c45e3d899f2f07f75f49871c7
kev <31861128+piyoki@users.noreply.github.com>
2026-03-11 16:05:10 -0400

    feat(home/pkg/dev/go): add proto-buf related pkgs

--- home/packages/development/language/golang/default.nix
+++ home/packages/development/language/golang/default.nix
@@ -3,6 +3,9 @@
 {
   home.packages = with pkgs; [
     golangci-lint # Fast linters Runner for Go
+    protobuf # Protocol Buffers - Google's data interchange format
+    protoc-gen-go # Go support for Google's protocol buffers
+    protoc-gen-go-grpc # Go support for gRPC
   ];
 
   programs.go.env = {
